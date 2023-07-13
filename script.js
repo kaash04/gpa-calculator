b2fc79e190239ac8a213d3b12c4269c7d60b1b49
Akash <2k21akash@gmail.com>
2023-07-13 14:26:53 +0530

    v2

---
+++ script.js
@@ -0,0 +1,18 @@
+class subject{
+    constructor(credit, gp)
+    {
+        this.credit = credit;
+        this.gp = gp;
+    }
+}
+
+document.getElementById("text1").innerHTML = "Basics (Elective):";
+document.getElementById("text2").innerHTML = "LAB - Basics:";
+document.getElementById("text3").innerHTML = "Communication Skill:";
+document.getElementById("text4").innerHTML = "LAB - Communication Skill:";
+document.getElementById("text5").innerHTML = "LAB - Workshop:";
+document.getElementById("text6").innerHTML = "Engineering Mathematics-II:";
+document.getElementById("text7").innerHTML = "Engineering Graphics & Design:";
+document.getElementById("text8").innerHTML = "LAB - EGD:";
+document.getElementById("text9").innerHTML = "Engineering Physics:";
+document.getElementById("text10").innerHTML = "LAB - Engineering Physics:";
